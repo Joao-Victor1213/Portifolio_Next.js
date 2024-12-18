77bb766768b4aa579efdc184ece00f1f34cf05fa
Joao Victor <joaooliverprofissional@gmail.com>
2024-12-18 14:29:25 -0300

    Adiçao das informações do banco de dados

--- FullStack/Quiz/pages/api/informacoesBd.ts
+++ FullStack/Quiz/pages/api/informacoesBd.ts
@@ -1,4 +1,4 @@
-const host = 'localhost', user = 'root', password = '', database = 'quiz'
+const host = 'srv1445.hstgr.io', user = 'u599479785_root', password = 'Ia@brasil15w23', database = 'u599479785_quiz'
 
 export default function InformacoesBd(){
     return {'host':host, 'user':user,'password':password, 'database':database}
