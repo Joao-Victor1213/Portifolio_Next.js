5320137fb8c00ae24aa479870f4fa574e1a00c69
Joao Victor <joaooliverprofissional@gmail.com>
2024-12-18 15:27:08 -0300

    Variaveis

--- FullStack/Quiz/pages/api/informacoesBd.ts
+++ FullStack/Quiz/pages/api/informacoesBd.ts
@@ -1,4 +1,4 @@
-const host = 'srv1445.hstgr.io', user = 'u599479785_root', password = 'Ia@brasil15w23', database = 'u599479785_quiz'
+const host = process.env.NEXT_PUBLIC_HOST, user = process.env.NEXT_PUBLIC_USER, password = process.env.NEXT_PUBLIC_PASSWORD, database = process.env.NEXT_PUBLIC_DATABASE
 
 export default function InformacoesBd(){
     return {'host':host, 'user':user,'password':password, 'database':database}
